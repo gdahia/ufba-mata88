7454d178d36c6afe1fa968c21a5fe6bc78487fea
Gabriel Dahia <gdahia@gmail.com>
2018-02-14 21:53:59 -0300

    add mutual exclusion access to replicas vision

--- Server.java
+++ Server.java
@@ -7,6 +7,10 @@ public interface Server extends Remote {
   public void connectNewReplica(Server newReplica) throws RemoteException;
   public String getHostname() throws RemoteException;
   public void addReplica(Server server) throws RemoteException;
+  public int registerReplicasRequest(String claimant, int timestamp) throws RemoteException;
+  public void requestReplicas() throws RemoteException;
+  public void registerReplicasRelease(String claimant, int timestamp) throws RemoteException;
+  public void releaseReplicas() throws RemoteException;
   public boolean addUser(String username, String userCredentials) throws RemoteException;
   public void removeUser(String username) throws RemoteException;
   public boolean addUserToChat(String username, Chat chat) throws RemoteException;
--- ServerImpl.java
+++ ServerImpl.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.rmi.RemoteException;
@@ -8,12 +9,18 @@ public class ServerImpl extends UnicastRemoteObject implements Server {
   private HashMap<String, String> credentials;
   private HashMap<String, Session> sessions;
   private ArrayList<Server> replicas;
+  private HashMap<String, Integer> replicasRequests;
+  private HashMap<String, Integer> replicasMessages;
+  private Integer replicasClock;
 
   public ServerImpl() throws RemoteException {
     hostname = System.getProperty("java.rmi.server.hostname");
     credentials = new HashMap<String, String>();
     sessions = new HashMap<String, Session>();
     replicas = new ArrayList<Server>();
+    replicasRequests = new HashMap<String, Integer>();
+    replicasMessages = new HashMap<String, Integer>();
+    replicasClock = 0;
   }
 
   public String getHostname() {
@@ -30,7 +37,117 @@ public class ServerImpl extends UnicastRemoteObject implements Server {
     }
   }
 
-  public synchronized void connectNewReplica(Server newReplica) {
+  public synchronized int registerReplicasRequest(String claimant, int timestamp) {
+    // register request
+    replicasRequests.put(claimant, timestamp);
+
+    // register message timestamp
+    replicasMessages.put(claimant, timestamp);
+
+    // update replicas clock
+    if (timestamp >= replicasClock)
+      replicasClock = timestamp + 1;
+
+    return replicasClock;
+  }
+
+  public void requestReplicas() {
+    int timestamp = replicasClock;
+
+    // send request message to replicas
+    for (Server replica : replicas) {
+      try {
+        int ack = replica.registerReplicasRequest(hostname, timestamp);
+
+        // register ack timestamp
+        replicasMessages.put(replica.getHostname(), ack);
+
+        // update replicas clock
+        synchronized (replicasClock) {
+          if (ack >= replicasClock)
+            replicasClock = timestamp + 1;
+          else
+            replicasClock++;
+        }
+
+      } catch (Exception e) {
+        System.err.println("ServerImpl, requestReplicas: " + e.toString());
+      }
+    }
+
+    // send request to itself
+    registerReplicasRequest(hostname, timestamp);
+  }
+
+  public synchronized void registerReplicasRelease(String claimant, int timestamp) {
+    // remove claimants requests
+    replicasRequests.remove(claimant);
+
+    // register message timestamp
+    replicasMessages.put(claimant, timestamp);
+
+    // update replicas clock
+    if (timestamp >= replicasClock)
+      replicasClock = timestamp + 1;
+  }
+
+  public void releaseReplicas() {
+    int timestamp = replicasClock;
+
+    // remove own request messages
+    registerReplicasRelease(hostname, timestamp);
+
+    // update replicas clock
+    synchronized (replicasClock) {
+      replicasClock++;
+    }
+
+    // send release message to replicas
+    for (Server replica : replicas) {
+      try {
+        replica.registerReplicasRelease(hostname, timestamp);
+
+        // update replicas clock
+        synchronized (replicasClock) {
+          replicasClock++;
+        }
+      } catch (Exception e) {
+        System.err.println("ServerImpl, releaseReplicas: " + e.toString());
+      }
+    }
+  }
+
+  public boolean replicasGranted() {
+    int timestamp = replicasRequests.get(hostname);
+
+    // check condition (i)
+    for (Map.Entry<String, Integer> entry : replicasRequests.entrySet()) {
+      int replicaTimestamp = entry.getValue();
+      String replicaHostname = entry.getKey();
+
+      if (replicaTimestamp < timestamp
+          || (replicaTimestamp == timestamp && replicaHostname.compareTo(hostname) < 0))
+        return false;
+    }
+
+    // check condition (ii)
+    for (Map.Entry<String, Integer> entry : replicasMessages.entrySet()) {
+      int replicaTimestamp = entry.getValue();
+      String replicaHostname = entry.getKey();
+
+      if (replicaTimestamp <= timestamp && replicaHostname != hostname)
+        return false;
+    }
+
+    return true;
+  }
+
+  public void connectNewReplica(Server newReplica) {
+    // wait until access to replicas is granted
+    requestReplicas();
+    while (!replicasGranted())
+      ;
+
     // connect to every other replica
     for (Server replica : replicas) {
       try {
@@ -48,9 +165,11 @@ public class ServerImpl extends UnicastRemoteObject implements Server {
     } catch (Exception e) {
       System.err.println("ServerImpl, connectNewReplica exception (2): " + e.toString());
     }
+
+    releaseReplicas();
   }
 
-  public synchronized void addReplica(Server server) {
+  public void addReplica(Server server) {
     try {
       String serverHostname = server.getHostname();
       replicas.add(server);
